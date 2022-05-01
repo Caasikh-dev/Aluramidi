bd1b638f36154f009bd90d7a5125c49b99508ab4
Caasikh-dev <89452379+Caasikh-dev@users.noreply.github.com>
2022-05-01 12:43:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+function tocaSom(seletorAudio) {
+    const elemento = document.querySelector(seletorAudio);
+
+    if(elemento && elemento.localName === 'audio'){ 
+        elemento.play()
+       }else{
+           console.log("elemento não encontrado ou seletor invalido")
+       }
+      
+
+}
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for (let contador = 0; contador<listaDeTeclas.length; contador++){
+
+    const tecla = listaDeTeclas[contador]
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`;  
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+    tecla.onkeydown = function (event){
+        console.log(event.code)
+        if(event.code === "Enter"||event.code === "Space")
+        tecla.classList.add('ativa');
+    }
+    tecla.onkeyup = function(event){
+        if(event.code === "Enter"||event.code === "Space")
+        tecla.classList.remove('ativa');
+    }
+}
+
+
+
+
